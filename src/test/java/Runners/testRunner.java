1bfcc63c20437e50c0debb39aa1c88183c7d9440
nurhayatkoklu <koklunurhayat@gmail.com>
2022-11-11 13:20:52 +0300

    nurhayat commit 2

--- src/test/java/Runners/testRunner.java
+++ src/test/java/Runners/testRunner.java
@@ -1,22 +1,31 @@
 package Runners;
 
+import com.aventstack.extentreports.service.ExtentService;
 import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Listeners;
 
 public class testRunner extends AbstractTestNGCucumberTests {
 
 
     @CucumberOptions(
-            tags = " ",
-            features = {"src/test/java/FeaturesFiles"},
+            tags = "@Nurhayat",
+            features = {"src/test/java/FeatureFiles"},
             glue = {"StepDefinitions"}
-
     )
 
     @Listeners({ExtentITestListenerClassAdapter.class})
     public class ExtentReports extends AbstractTestNGCucumberTests {
+        @AfterClass
+        public static void writeExtentReport() {
+            ExtentService.getInstance().setSystemInfo("User Name", "Nurhayat");
+            ExtentService.getInstance().setSystemInfo("Application Name", "amazon");
+            ExtentService.getInstance().setSystemInfo("Operating System Info", System.getProperty("os.name").toString());
+            ExtentService.getInstance().setSystemInfo("Department", "Automation Test Group");
+            ExtentService.getInstance().setSystemInfo("Team Members", "8 Passionate Automation Testers");
+        }
 
 
 
